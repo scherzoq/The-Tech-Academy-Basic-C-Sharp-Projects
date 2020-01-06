8a6dc2e0b4b8cb8671b0926f62890011b2996f61
scherzoq <johntaichin@gmail.com>
2020-01-06 13:36:40 -0500

    Student Daily Report project
    Student Daily Report project

---
+++ StudentDailyReport/StudentDailyReport/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StudentDailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            
+            Console.WriteLine("What course are you on?");
+            string currentCourse = Console.ReadLine();
+
+            Console.WriteLine("What page number are you on?");
+            string page = Console.ReadLine();
+            int pageNumber = Convert.ToInt32(page);
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+            string help = Console.ReadLine();
+            bool helpStatus = Convert.ToBoolean(help);
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string positiveExp = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            int hoursStudied = Convert.ToInt32(hours);
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
