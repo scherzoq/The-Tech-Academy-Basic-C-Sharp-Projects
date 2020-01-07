5da7e7fde99d3acabc7a441cc03a35f3e5fa0336
scherzoq <johntaichin@gmail.com>
2020-01-07 16:56:46 -0500

    string function exercises
    string function exercises

---
+++ StringExercise_p97/StringExercise_p97/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExercise_p97
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // concatenate strings and use "ToUpper" method
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            name = name.ToUpper();
+            Console.WriteLine("Where do you live (enter name of either city or state)?");
+            string place = Console.ReadLine();
+            Console.WriteLine("Enter an adjective to describe this place:");
+            string adjective = Console.ReadLine();
+            string greeting = "Hello, " + name + " of " + adjective + " " + place + "!";
+            Console.WriteLine(greeting);
+            Console.WriteLine("(press enter to continue)");
+            Console.ReadLine();
+
+            // StringBuilder
+            StringBuilder mobyDick = new StringBuilder();
+            mobyDick.Append("Call me Ishmael. ");
+            mobyDick.Append("Some years ago - never mind how long precisely - having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. ");
+            mobyDick.Append("It is a way I have of driving off the spleen and regulating the circulation.");
+            Console.WriteLine(mobyDick);
+            Console.Read();
+        }
+    }
+}
---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            string quote = "The man said, \"Hello, John.\"\nHello on a new line.\n\tHello on a tab.";
+            // string fileName = "C:\\Users\\John";
+            string fileName = @"C:\Users\John";
+
+            // bool trueOrFalse = name.Contains("h");
+            bool trueOrFalse = name.EndsWith("h");
+
+            int length = name.Length;
+
+            name = name.ToUpper();
+
+            Console.WriteLine(quote);
+            Console.WriteLine(fileName);
+            Console.WriteLine(trueOrFalse);
+            Console.WriteLine(length);
+            Console.WriteLine(name);
+            
+            // create instance of class StringBuilder:
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is John");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
