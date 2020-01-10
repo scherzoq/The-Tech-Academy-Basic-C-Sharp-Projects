d2ad99c83c50f7454d0ae812309f975008e818e3
scherzoq <johntaichin@gmail.com>
2020-01-10 17:53:05 -0500

    Classes and methods: method overloading
    Method overloading exercises

---
+++ ClassesAndMethodsExercise_p159/ClassesAndMethodsExercise_p159/MathOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsExercise_p159
+{
+    public class MathOperations
+    {
+        private static int num;
+
+        public static int mathOp(int number)
+        {
+            return number * 2;
+        }
+
+        public static double mathOp(double number)
+        {
+            return number / 2;
+        }
+
+        public static int mathOp(string number)
+        {
+            num = Convert.ToInt32(number);
+            return num + 25000;
+        }
+    }
+}
---
+++ ClassesAndMethodsExercise_p159/ClassesAndMethodsExercise_p159/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsExercise_p159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = MathOperations.mathOp(50);
+            Console.WriteLine(numberOne);
+
+            double numberTwo = MathOperations.mathOp(25.43);
+            Console.WriteLine(numberTwo);
+
+            int numberThree = MathOperations.mathOp("9");
+            Console.WriteLine(numberThree);
+
+            Console.ReadLine();
+        }
+    }
+}
