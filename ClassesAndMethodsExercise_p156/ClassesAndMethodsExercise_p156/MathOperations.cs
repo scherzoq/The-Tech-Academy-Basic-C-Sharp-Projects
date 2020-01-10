782f921ae805d5eec60dc37565c288e771941e34
scherzoq <johntaichin@gmail.com>
2020-01-10 16:54:30 -0500

    Classes and Methods
    Classes and methods, initial exercises

---
+++ ClassesAndMethodsExercise_p156/ClassesAndMethodsExercise_p156/MathOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsExercise_p156
+{
+    public class MathOperations
+    {
+        public static int MultiplyByThree(int number)
+        {
+            return number * 3;
+        }
+
+        public static int SubtractFifty(int number)
+        {
+            return number - 50;
+        }
+
+        public static int AddTenThousand(int number)
+        {
+            return number + 10000;
+        }
+    }
+}
---
+++ ClassesAndMethodsExercise_p156/ClassesAndMethodsExercise_p156/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsExercise_p156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(number + " multiplied by 3 equals " + MathOperations.MultiplyByThree(number));
+            Console.WriteLine(number + " minus 50 equals " + MathOperations.SubtractFifty(number));
+            Console.WriteLine(number + " plus 10000 equals " + MathOperations.AddTenThousand(number));
+
+            Console.ReadLine();
+        }
+    }
+}
