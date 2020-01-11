44a120279746457a0b4b703176445dc144acdfa2
scherzoq <johntaichin@gmail.com>
2020-01-11 18:20:08 -0500

    Classes and Methods, optional parameter exercise
    Classes and Methods, optional parameter exercise

---
+++ ClassesAndMethodsExercise_p160/ClassesAndMethodsExercise_p160/MathOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsExercise_p160
+{
+    public class MathOperation
+    {
+        public static int mathOp(int numberOne, int numberTwo=2)
+        {
+            return (numberOne * 3 + numberTwo * 5);
+        }
+    }
+}
---
+++ ClassesAndMethodsExercise_p160/ClassesAndMethodsExercise_p160/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsExercise_p160
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a first number:");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number (optional)");
+            try
+            {
+                int numTwo = Convert.ToInt32(Console.ReadLine());
+                int answer = MathOperation.mathOp(numOne, numTwo);
+                Console.WriteLine(numOne + " times 3 plus " + numTwo + " times 5 equals " + answer);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("No number entered; default of 2 used");
+                int answer = MathOperation.mathOp(numOne);
+                Console.WriteLine(numOne + " times 3 plus 2 times 5 equals " + answer);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
