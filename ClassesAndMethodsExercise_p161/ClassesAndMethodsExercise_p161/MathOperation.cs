90bc22cd23f6552fbf81d03b005ee7638273325a
scherzoq <johntaichin@gmail.com>
2020-01-11 18:35:43 -0500

    Classes and methods: void method exercise
    Classes and methods: void method exercise

---
+++ ClassesAndMethodsExercise_p161/ClassesAndMethodsExercise_p161/MathOperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsExercise_p161
+{
+    public class MathOperation
+    {
+        public static void mathOp(int numberOne, int numberTwo)
+        {
+            Console.WriteLine(numberOne * 2 + 100);
+            Console.WriteLine(numberTwo);
+        }
+    }
+}
---
+++ ClassesAndMethodsExercise_p161/ClassesAndMethodsExercise_p161/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsExercise_p161
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation.mathOp(3, 4);
+            MathOperation.mathOp(numberOne: 5, numberTwo: 32);
+            Console.ReadLine();
+        }
+    }
+}
