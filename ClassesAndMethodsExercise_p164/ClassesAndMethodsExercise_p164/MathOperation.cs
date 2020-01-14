98874a78845bb3ec81aa2440b064192bce791579
scherzoq <johntaichin@gmail.com>
2020-01-14 14:55:37 -0500

    classes and methods: output parameters
    classes and methods: output parameters

---
+++ ClassesAndMethodsExercise_p164/ClassesAndMethodsExercise_p164/MathOperation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsExercise_p164
+{
+    public class MathOperation
+    {
+        public static void DivideByTwo(int number, out int remainder)
+        {
+            Console.WriteLine(number + " divided by two equals " + number / 2);
+            remainder = number % 2;
+        }
+
+        public static void DivideByTwo(string number, out int remainder)
+        {
+            int num = Convert.ToInt32(number);
+            Console.WriteLine(num + " divided by two equals " + num / 2);
+            remainder = num % 2;
+        }
+    }
+}
---
+++ ClassesAndMethodsExercise_p164/ClassesAndMethodsExercise_p164/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsExercise_p164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation.DivideByTwo(5, out int rem1);
+            Console.WriteLine("The remainder is " + rem1);
+            Console.ReadLine();
+
+            MathOperation.DivideByTwo(20, out int rem2);
+            Console.WriteLine("The remainder is " + rem2);
+            Console.ReadLine();
+        }
+    }
+}
