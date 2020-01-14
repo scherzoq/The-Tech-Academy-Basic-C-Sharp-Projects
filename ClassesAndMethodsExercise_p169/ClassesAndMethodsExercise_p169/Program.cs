ae4117b601451135f629fb151d8312043f59eb66
scherzoq <johntaichin@gmail.com>
2020-01-14 15:53:20 -0500

    Classes and methods: inheritance
    Classes and methods: inheritance

---
+++ ClassesAndMethodsExercise_p169/ClassesAndMethodsExercise_p169/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsExercise_p169
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee test = new Employee() { FirstName = "Sample", LastName = "Student" };
+            test.SayName();
+            Console.ReadLine();
+        }
+    }
+}
