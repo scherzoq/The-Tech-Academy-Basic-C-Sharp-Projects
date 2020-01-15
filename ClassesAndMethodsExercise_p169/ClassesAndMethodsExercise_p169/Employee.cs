03b7bcaa4267c01b83da82550928a4d84e00f395
scherzoq <johntaichin@gmail.com>
2020-01-15 11:10:14 -0500

    operator overloading
    operator overloading

--- ClassesAndMethodsExercise_p169/ClassesAndMethodsExercise_p169/Employee.cs
+++ ClassesAndMethodsExercise_p169/ClassesAndMethodsExercise_p169/Employee.cs
@@ -9,5 +9,16 @@ namespace ClassesAndMethodsExercise_p169
     public class Employee : Person
     {
         public int Id { get; set; }
+
+        // operator overloading added for p. 184 assignment
+        public static bool operator == (Employee firstEmployee, Employee secondEmployee)
+        {
+            return firstEmployee.Id == secondEmployee.Id;
+        }
+
+        public static bool operator != (Employee firstEmployee, Employee secondEmployee)
+        {
+            return firstEmployee.Id != secondEmployee.Id;
+        }
     }
 }
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,10 +10,12 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "Jesse", "Bill", "Bob" };
-            game.ListPlayers();
-            Console.ReadLine();
+            Game game = new TwentyOneGame();
+            game.Players = new List<Player>();
+            Player player = new Player();
+            player.Name = "Jesse";
+            game += player;
+            game -= player;
 
             Deck deck = new Deck();
             deck.Shuffle(3);
