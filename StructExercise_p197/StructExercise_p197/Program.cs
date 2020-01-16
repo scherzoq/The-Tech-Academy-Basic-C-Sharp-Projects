c209cd7a37bae3c5e8421e874919cf497bc470aa
scherzoq <johntaichin@gmail.com>
2020-01-16 18:13:35 -0500

    structs
    structs

---
+++ StructExercise_p197/StructExercise_p197/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExercise_p197
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number sample = new Number();
+            sample.Amount = 3.14159M;
+            Console.WriteLine(sample.Amount);
+            Console.ReadLine();
+        }
+    }
+}
