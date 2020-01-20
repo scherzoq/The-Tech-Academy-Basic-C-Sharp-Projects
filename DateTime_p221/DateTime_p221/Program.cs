632ef6e9523575d910179c4058769edd635f454f
scherzoq <johntaichin@gmail.com>
2020-01-20 14:00:03 -0500

    DateTime
    DateTime

---
+++ DateTime_p221/DateTime_p221/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_p221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.WriteLine("Enter a number:");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            DateTime future = now.AddHours(hours);
+            Console.WriteLine("In {0} hours it will be {1}", hours, future);
+            Console.ReadLine();
+        }
+    }
+}
