242b8e43f3f9c686734d61c38558aceff18ca7e8
scherzoq <johntaichin@gmail.com>
2020-01-22 23:31:21 -0500

    exception handling
    exception handling

---
+++ ExceptionHandlingExercise_p235/ExceptionHandlingExercise_p235/AgeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingExercise_p235
+{
+    public class AgeException : Exception
+    {
+        public AgeException()
+            : base() { }
+        public AgeException(string message)
+            : base(message) { }
+    }
+}
---
+++ ExceptionHandlingExercise_p235/ExceptionHandlingExercise_p235/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingExercise_p235
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("How old are you?");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age <= 0) throw new AgeException();
+                int year1 = Convert.ToInt32(DateTime.Now.Year) - age;
+                int year2 = year1 - 1;
+                Console.WriteLine("You were born in {0} or {1}", year2, year1);
+            }
+            catch (AgeException)
+            {
+                Console.WriteLine("Please enter a valid age.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occurred. Please contact your System Administrator.");                
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
